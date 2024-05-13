7f8cb4c172cd5cf3ff1a592e82e3b7dbd184e6df
Fish <sm29729443@gmail.com>
2024-05-13 12:18:20 +0800

    spring ioc 無參contructor、靜態、非靜態工廠 xml 配置範例

---
+++ spring-ioc-xml-01/src/main/java/com/atguigu/ioc_01/ClientServiceImpl.java
@@ -0,0 +1,8 @@
+package com.atguigu.ioc_01;
+
+/**
+ * ClassName: ClientServiceImpl
+ * Package: com.atguigu.ioc_01
+ */
+public class ClientServiceImpl {
+}
---
+++ spring-ioc-xml-01/src/main/java/com/atguigu/ioc_01/HappyComponent.java
@@ -0,0 +1,11 @@
+package com.atguigu.ioc_01;
+
+
+public class HappyComponent {
+
+    //默認包含了無參Contructor
+
+    public void doWork() {
+        System.out.println("HappyComponent.doWork");
+    }
+}
