c4d0b5fd9665857820b2eb6965252aabd4e3f315
Fish <sm29729443@gmail.com>
2024-05-13 13:47:09 +0800

    創建 IOC 容器與從 IOC 得到 Bean 的範例

---
+++ spring-ioc-xml-01/src/main/java/com/atguigu/ioc_03/HappyComponent.java
@@ -0,0 +1,11 @@
+package com.atguigu.ioc_03;
+
+
+public class HappyComponent {
+
+    //默認包含了無參Contructor
+
+    public void doWork() {
+        System.out.println("HappyComponent.doWork");
+    }
+}
